2122e06ec6b5d2406f969ae178810528ed46a19d
Anders Kalhauge <anders@kalhauge.dk>
2015-02-12 12:17:22 +0100

    Added jMock test

---
+++ src/test/java/dk/kalhauge/qed/QedHandlerTest.java
@@ -0,0 +1,24 @@
+package dk.kalhauge.qed;
+
+import com.sun.net.httpserver.HttpExchange;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Test;
+
+
+
+public class QedHandlerTest {
+  Mockery context = new JUnitRuleMockery();
+  
+  @Test
+  public void testBuildScript() throws Exception {
+    final HttpExchange exhange = context.mock(HttpExchange.class);
+    TestFacade facade = new TestFacade();
+    QedHandler handler = new QedHandler(facade);
+    handler.handle(exhange);
+    }
+  
+  
+  
+  
+}
